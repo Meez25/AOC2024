82bf03b13b37c1e4a45113850c69cc41f86ee042
Yann BRONNER <yann.bronner@gmail.com>
2024-12-20 22:52:11 +0100

    refactor

--- day4/day4.go
+++ day4/day4.go
@@ -4,9 +4,11 @@ import (
 	"bytes"
 	"fmt"
 	"os"
+	"time"
 )
 
-func dayFour() {
+func main() {
+	start := time.Now()
 	input, _ := os.ReadFile("day4input.txt")
 	lines := bytes.Split(input, []byte("\n"))
 
@@ -19,49 +21,44 @@ func dayFour() {
 			if string(lines[i][y]) == "X" {
 				// Check left
 				if y > 2 && string(lines[i][y-1]) == "M" && string(lines[i][y-2]) == "A" && string(lines[i][y-3]) == "S" {
-					fmt.Println("line:", i, "row:", y)
 					match++
 				}
 				// Check right
 				if y < len(lines[i])-3 && string(lines[i][y+1]) == "M" && string(lines[i][y+2]) == "A" && string(lines[i][y+3]) == "S" {
-					fmt.Println("line:", i, "row:", y)
 					match++
 				}
 				// Check up
 				if i > 2 && string(lines[i-1][y]) == "M" && string(lines[i-2][y]) == "A" && string(lines[i-3][y]) == "S" {
-					fmt.Println("line:", i, "row:", y)
 					match++
 				}
 				// Check down
 				if i < len(lines)-3 && string(lines[i+1][y]) == "M" && string(lines[i+2][y]) == "A" && string(lines[i+3][y]) == "S" {
-					fmt.Println("line:", i, "row:", y)
 					match++
 				}
 				// Check up right
 				if i > 2 && y < len(lines[i])-3 && string(lines[i-1][y+1]) == "M" && string(lines[i-2][y+2]) == "A" && string(lines[i-3][y+3]) == "S" {
 					match++
-					fmt.Println("line:", i, "row:", y)
 				}
 				// Check up left
 				if i > 2 && y > 2 && string(lines[i-1][y-1]) == "M" && string(lines[i-2][y-2]) == "A" && string(lines[i-3][y-3]) == "S" {
-					fmt.Println("line:", i, "row:", y)
 					match++
 				}
 				// Check down right
 				if i < len(lines)-3 && y < len(lines[i])-3 && string(lines[i+1][y+1]) == "M" && string(lines[i+2][y+2]) == "A" && string(lines[i+3][y+3]) == "S" {
 					match++
-					fmt.Println("line:", i, "row:", y)
 				}
 				// Check down left
 				if i < len(lines)-3 && y > 2 && string(lines[i+1][y-1]) == "M" && string(lines[i+2][y-2]) == "A" && string(lines[i+3][y-3]) == "S" {
 					match++
-					fmt.Println("line:", i, "row:", y)
 				}
 			}
 		}
 	}
 
+	elapsed := time.Since(start)
+	fmt.Println("Part 1 :", match, "in", elapsed)
 	matchPart2 := 0
+	start = time.Now()
 
 	for i := 0; i < len(lines); i++ {
 		for y := 0; y < len(lines[i]); y++ {
@@ -78,6 +75,6 @@ func dayFour() {
 		}
 	}
 
-	fmt.Println("part1: ", match)
-	fmt.Println("part2: ", matchPart2)
+	elapsed = time.Since(start)
+	fmt.Println("Part 2 :", matchPart2, "in", elapsed)
 }
